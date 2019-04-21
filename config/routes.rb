5f5ba36db686cf578a06483c2dbbcc3e7b0ca06e
Peter Shengqian Liu <shengqian.liu@appfolio.com>
2019-04-20 18:49:50 -0700

    psl - update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  mount Spina::Engine => '/blog'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   resources :health_check, only: :index
+
+  mount Spina::Engine => '/'
 end
